2a1c6974aaf9e239233b3110b0b87fd20c140c84
han <han@fbrns.co>
2025-06-27 00:58:31 +0700

    feat: setup mixpanel analytics

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import mixpanel from 'mixpanel-browser'
+
 import './index.css'
 import App from './App.tsx'
+
+const MIXPANEL_TOKEN = import.meta.env.VITE_MIXPANEL_TOKEN;
+
+if (MIXPANEL_TOKEN) {
+  mixpanel.init(MIXPANEL_TOKEN, {
+    debug: true,
+    track_pageview: true,
+    persistence: 'localStorage'
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
