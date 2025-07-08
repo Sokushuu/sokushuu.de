cdf0ab51c63c987f20574592a1acf7155368f499
han <han@fbrns.co>
2025-07-09 05:51:42 +0700

    feat: add posthog analytics integration

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import { usePostHog } from 'posthog-js/react'
 import {
   EcosystemOverview,
   HeroSection,
@@ -24,9 +26,23 @@ function LandingPage() {
   )
 }
 
+// Component to track page views
+function PageViewTracker() {
+  const location = useLocation()
+  const posthog = usePostHog()
+
+  useEffect(() => {
+    // Track pageview with PostHog when route changes
+    posthog?.capture('$pageview')
+  }, [location, posthog])
+
+  return null
+}
+
 function App() {
   return (
     <Router>
+      <PageViewTracker />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/survey/pengguna-airdrop" element={<SurveyPage />} />
